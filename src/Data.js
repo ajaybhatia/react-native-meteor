52a403c665b1aae018b30ede24a282f290a61f5a
ajaybhatia <prof.ajaybhatia@gmail.com>
2019-08-25 11:27:25 +0530

    Replaces minimongo-cache to @ajaybhatia/minimongo-cache and update other packages

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,5 @@
 import ReactNative from 'react-native/Libraries/Renderer/shims/ReactNative';
-import minimongo from 'minimongo-cache';
+import minimongo from '@ajaybhatia/minimongo-cache';
 import Trackr from 'trackr';
 import { InteractionManager } from 'react-native';
 process.nextTick = setImmediate;
