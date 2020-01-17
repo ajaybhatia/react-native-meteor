822fe167de76c68c7e530f4ecad5ced09768ea92
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-01-18 00:08:47 +0530

    Revert to depreciated method UNSAFE_componentWillUpdate

--- src/components/ReactMeteorData.js
+++ src/components/ReactMeteorData.js
@@ -4,63 +4,6 @@ import EJSON from 'ejson';
 import Data from '../Data';
 import MeteorDataManager from './MeteorDataManager';
 
-class ReactComponent extends React.Component {
-  componentDidMount() {
-    Data.waitDdpReady(() => {
-      if (this.getMeteorData) {
-        this.data = {};
-        this._meteorDataManager = new MeteorDataManager(this);
-        const newData = this._meteorDataManager.calculateData();
-        this._meteorDataManager.updateData(newData);
-      }
-    });
-  }
-
-  static getDerivedStateFromProps(nextProps, nextState) {
-    if (this.startMeteorSubscriptions) {
-      if (
-        !EJSON.equals(this.state, nextState) ||
-        !EJSON.equals(this.props, nextProps)
-      ) {
-        this._meteorSubscriptionsManager._meteorDataChangedCallback();
-      }
-    }
-
-    if (this.getMeteorData) {
-      const saveProps = this.props;
-      const saveState = this.state;
-      let newData;
-      try {
-        // Temporarily assign this.state and this.props,
-        // so that they are seen by getMeteorData!
-        // This is a simulation of how the proposed Observe API
-        // for React will work, which calls observe() after
-        // componentWillUpdate and after props and state are
-        // updated, but before render() is called.
-        // See https://github.com/facebook/react/issues/3398.
-        this.props = nextProps;
-        this.state = nextState;
-        newData = this._meteorDataManager.calculateData();
-      } finally {
-        this.props = saveProps;
-        this.state = saveState;
-      }
-
-      this._meteorDataManager.updateData(newData);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this._meteorDataManager) {
-      this._meteorDataManager.dispose();
-    }
-
-    if (this._meteorSubscriptionsManager) {
-      this._meteorSubscriptionsManager.dispose();
-    }
-  }
-}
-
 class ReactPureComponent extends React.PureComponent {
   componentDidMount() {
     Data.waitDdpReady(() => {
@@ -73,7 +16,7 @@ class ReactPureComponent extends React.PureComponent {
     });
   }
 
-  static getDerivedStateFromProps(nextProps, nextState) {
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
     if (this.startMeteorSubscriptions) {
       if (
         !EJSON.equals(this.state, nextState) ||
@@ -126,11 +69,10 @@ export default function connect(options) {
     };
   }
 
-  const { getMeteorData, pure = true } = expandedOptions;
+  const { getMeteorData } = expandedOptions;
 
-  const BaseComponent = pure ? ReactPureComponent : ReactComponent;
   return WrappedComponent =>
-    class ReactMeteorDataComponent extends BaseComponent {
+    class ReactMeteorDataComponent extends ReactPureComponent {
       getMeteorData() {
         return getMeteorData(this.props);
       }
